f7cd161e004304e5f685c76a3cb1a08524a075b7
Yang Chen (yangch3) <yangch3@cisco.com>
2018-11-19 16:41:49 +0800

    Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.

--- number_remove_k.py
+++ number_remove_k.py
@@ -8,47 +8,31 @@ def number_remove_k_1(num, k):
 
     n_length = len(n_list)
     after_list = [] #可以看成是栈结构，最后一个是栈顶元素
-    after_n = n_length - k
-    if after_n <= 0:
+    new_length = n_length - k
+    if new_length <= 0:
         return "0"
 
-    for i in range(n_length):
-        if k == 0:
-            m = after_n - len(after_list)
-            after_list.extend(n_list[i:i+m+1])
-            break
+    top_index = 0
+    for i in n_list:
+        while top_index > 0 and k > 0 and after_list[top_index-1] > i:
+            k = k-1
+            after_list.pop(top_index-1)
+            top_index = top_index -1
+        after_list.append(i)
+        top_index = top_index + 1
 
-        if len(after_list) == 0:
-            after_list.append(n_list[i])
-        else:
-            # 和栈after_list的顶元素比较，如果比它小，则栈顶元素出栈，接续比下一个栈顶元素，直到k为0或者不满足条件，入栈。
-            compare_n = after_list[len(after_list)-1]
-            while n_list[i] < compare_n:
-                after_list.pop(len(after_list)-1)
-                k = k-1
-                if len(after_list) == 0 or k == 0:
-                    break
-                compare_n = after_list[len(after_list)-1]
-            after_list.append(n_list[i])
+    index_zero = 0
+    while index_zero < new_length and after_list[index_zero] == 0:
+        index_zero = index_zero + 1
 
-    res = after_list[:after_n]
-    y = 0
-    l = len(res)
-    for j in range(l):
-        if res[j] == 0:
-            if y == l-1:
-                y=j
-                break
-            else:
-                y=j+1
+    if index_zero == new_length:
+        return "0"
 
-        else:
-            break
-    return ''.join(list(map(str, res[y:])))
+    return ''.join(list(map(str, after_list[index_zero:new_length])))
 
 
 
 if __name__ == '__main__':
     # res = number_remove_k_1("78912", 2)
-    res = number_remove_k_1("100", 1)
+    res = number_remove_k_1("112", 1)
     print(res)
